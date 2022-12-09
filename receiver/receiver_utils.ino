c76144232497e2181ee996b338ced457ea496972
Jared Dunn <jdunn@webucator.com>
2022-12-09 18:04:37 -0500

    docs: add comments to receiver util functions

--- receiver/receiver_utils.ino
+++ receiver/receiver_utils.ino
@@ -8,29 +8,39 @@ Servo servo_test;        //initialize a servo object for the connected servo
 const int rs = 0, en = 1, d4 = 2, d5 = 3, d6 = 4, d7 = 5;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
+/**
+ * Initializes the LCD.
+ */
 void initializeLCD() {
   lcd.begin(16,2);
 }
 
 /**
- * Clears the old LCD message and writes a new message.
+ * Clears the old LCD message and writes a new message and heartbeat.
 */
 void writeToLCD(String msg, int hb) {
   lcd.clear();
   lcd.print(msg);
   lcd.setCursor(0, 1);
-  if(hb != 404){
+  if(msg != "OFF" || msg != "ERROR"){
     lcd.print(hb);
   }
 }
 
+// MOTOR
+
+/**
+ * Initialized the motor.
+*/
 void initializeMotor(){
   servo_test.attach(9);
   servo_test.write(0);
   delay(1000);
 }
 
-// MOTOR
+/**
+ * Update the motor to a specific position.
+*/
 void updateMotor(int rotation) {
   servo_test.write(rotation); 
   delay(1000);
